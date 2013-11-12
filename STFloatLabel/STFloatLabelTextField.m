02cc55afec1380f07c496a9d06ce11a73fa2e383
Scott Talbot <s@chikachow.org>
2013-11-12 20:10:09 +1100

    Tweak shrunken size, animate scaling from placeholder text

--- STFloatLabel/STFloatLabelTextField.m
+++ STFloatLabel/STFloatLabelTextField.m
@@ -7,30 +7,36 @@
 #import "STFloatLabelTextField.h"
 
 
+static CGFloat const STFloatLabelTextFieldPlaceholderEnlargedAlpha = .5;
+static CGFloat const STFloatLabelTextFieldPlaceholderShrunkenAlpha = 1;
+
+
 @implementation STFloatLabelTextField {
 @private
-    UILabel *_shrunkenPlaceholderLabel;
-    BOOL _shrunkenPlaceholderVisible;
+    UILabel *_placeholderLabel;
+    BOOL _placeholderShrunken;
 }
 
 - (id)initWithFrame:(CGRect)frame {
     if ((self = [super initWithFrame:frame])) {
         CGRect const bounds = self.bounds;
 
-        _shrunkenPlaceholderLabel = [[UILabel alloc] initWithFrame:bounds];
-        _shrunkenPlaceholderLabel.autoresizingMask = UIViewAutoresizingFlexibleWidth|UIViewAutoresizingFlexibleHeight;
-        _shrunkenPlaceholderLabel.textColor = [UIColor colorWithWhite:.7 alpha:1];
-        _shrunkenPlaceholderLabel.backgroundColor = [UIColor clearColor];
-        [self addSubview:_shrunkenPlaceholderLabel];
+        self.clipsToBounds = NO;
 
-        _shrunkenPlaceholderLabel.alpha = 0;
+        _placeholderLabel = [[UILabel alloc] initWithFrame:bounds];
+        _placeholderLabel.autoresizingMask = UIViewAutoresizingFlexibleWidth|UIViewAutoresizingFlexibleHeight;
+        _placeholderLabel.textColor = [UIColor colorWithWhite:.7 alpha:1];
+        _placeholderLabel.backgroundColor = [UIColor clearColor];
+        [self addSubview:_placeholderLabel];
+
+        [self st__setPlaceholderShrunken:NO animated:NO];
     }
     return self;
 }
 
 - (void)tintColorDidChange {
     UIColor * const tintColor = self.tintColor;
-    [_shrunkenPlaceholderLabel setHighlightedTextColor:tintColor];
+    [_placeholderLabel setHighlightedTextColor:tintColor];
 }
 
 
@@ -47,53 +53,66 @@
 //    .attributedPlaceholder unsupported
 //    NSAttributedString * const attributedPlaceholderText = self.attributedPlaceholder;
 //    if (attributedPlaceholderText) {
-//        _shrunkenPlaceholderLabel.attributedText = attributedPlaceholderText;
+//        _placeholderLabel.attributedText = attributedPlaceholderText;
 //        return;
 //    }
     NSString * const placeholderText = self.placeholder;
-    _shrunkenPlaceholderLabel.text = placeholderText;
+    _placeholderLabel.text = placeholderText;
 }
 
 - (BOOL)becomeFirstResponder {
-    [_shrunkenPlaceholderLabel setHighlighted:YES];
+    [_placeholderLabel setHighlighted:YES];
     return [super becomeFirstResponder];
 }
 
 - (BOOL)resignFirstResponder {
-    [_shrunkenPlaceholderLabel setHighlighted:NO];
+    [_placeholderLabel setHighlighted:NO];
     return [super resignFirstResponder];
 }
+
+- (void)drawPlaceholderInRect:(CGRect)rect {}
 
 - (void)layoutSubviews {
     [super layoutSubviews];
 
-    CGRect const bounds = self.bounds;
-
-    CGFloat const shrunkenPlaceholderLabelScale = 3./8.;
-    CGFloat const shrunkenPlaceholderLabelTranslation = -(.5 - (shrunkenPlaceholderLabelScale / 2.));
-    CGFloat const shrunkenPlaceholderLabelTranslationX = CGRectGetWidth(bounds) * shrunkenPlaceholderLabelTranslation;
-    CGFloat const shrunkenPlaceholderLabelTranslationY = CGRectGetHeight(bounds) * shrunkenPlaceholderLabelTranslation;
-    CGAffineTransform transform = CGAffineTransformMakeTranslation(shrunkenPlaceholderLabelTranslationX, shrunkenPlaceholderLabelTranslationY);
-    transform = CGAffineTransformScale(transform, shrunkenPlaceholderLabelScale, shrunkenPlaceholderLabelScale);
-    _shrunkenPlaceholderLabel.transform = transform;
-
     BOOL const hasText = self.text.length > 0;
-    [self st_setShrunkenPlaceholderVisible:hasText animated:YES];
+    [self st_setPlaceholderShrunken:hasText animated:YES];
 }
 
-- (void)st_setShrunkenPlaceholderVisible:(BOOL)visible animated:(BOOL)animated {
-    if (visible != _shrunkenPlaceholderVisible) {
-        _shrunkenPlaceholderVisible = visible;
-
-        void (^animations)(void) = ^{
-            _shrunkenPlaceholderLabel.alpha = visible ? 1 : 0;
-        };
-        if (animated) {
-            [UIView animateWithDuration:.1 delay:0 options:UIViewAnimationOptionAllowUserInteraction|UIViewAnimationOptionBeginFromCurrentState animations:animations completion:nil];
-        } else {
-            animations();
-        }
+- (void)st_setPlaceholderShrunken:(BOOL)shrunken animated:(BOOL)animated {
+    if (shrunken != _placeholderShrunken) {
+        [self st__setPlaceholderShrunken:shrunken animated:animated];
     }
+}
+- (void)st__setPlaceholderShrunken:(BOOL)shrunken animated:(BOOL)animated {
+    _placeholderShrunken = shrunken;
+
+    CGRect const bounds = self.bounds;
+    CGAffineTransform const placeholderLabelTransform = [self.class st_placeholderTransformForBounds:bounds shrunken:shrunken];
+    CGFloat const placeholderLabelAlpha = shrunken ? STFloatLabelTextFieldPlaceholderShrunkenAlpha : STFloatLabelTextFieldPlaceholderEnlargedAlpha;
+
+    void (^animations)(void) = ^{
+        _placeholderLabel.alpha = placeholderLabelAlpha;
+        _placeholderLabel.transform = placeholderLabelTransform;
+    };
+    if (animated) {
+        [UIView animateWithDuration:1./8. delay:0 options:UIViewAnimationOptionAllowUserInteraction|UIViewAnimationOptionBeginFromCurrentState animations:animations completion:nil];
+    } else {
+        animations();
+    }
+}
+
++ (CGAffineTransform)st_placeholderTransformForBounds:(CGRect)bounds shrunken:(BOOL)shrunken {
+    CGFloat const placeholderLabelScale = .5;
+    CGFloat const placeholderLabelTranslation = -(.5 - (placeholderLabelScale / 2.));
+    CGFloat const placeholderLabelTranslationX = CGRectGetWidth(bounds) * placeholderLabelTranslation;
+    CGFloat const placeholderLabelTranslationY = CGRectGetHeight(bounds) * placeholderLabelTranslation - 6;
+    CGAffineTransform placeholderLabelTransform = CGAffineTransformIdentity;
+    if (shrunken) {
+        placeholderLabelTransform = CGAffineTransformMakeTranslation(placeholderLabelTranslationX, placeholderLabelTranslationY);
+        placeholderLabelTransform = CGAffineTransformScale(placeholderLabelTransform, placeholderLabelScale, placeholderLabelScale);
+    }
+    return placeholderLabelTransform;
 }
 
 @end
